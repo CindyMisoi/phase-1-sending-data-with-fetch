fcfe3eef81cc8faae0017e06a9d84758c6aa33ba
CindyMisoi <crmisoi2k@gmail.com>
2022-12-10 18:23:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+let userData = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+function submitData() {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function(error){
+        alert(`Error!`)
+        console.log(error.message);
+    })
+}
+submitData();
